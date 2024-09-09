6c8e6b1eca1438c6905fe6f09f11e652d70e4052
windy <windywu812@gmail.com>
2024-09-09 20:48:47 +0700

    Initial Commit

---
+++ TaskContext/TaskContextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskContextApp.swift
+//  TaskContext
+//
+//  Created by Windy on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskContextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
